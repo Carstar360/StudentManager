4b8fd963e7e68e3701379cef6a886a59be0e9df2
Carstar360 <clee124@ecc-jaguar.csuchico.edu>
2019-12-06 10:48:07 -0800

    ok

---
+++ student.cpp
@@ -0,0 +1,18 @@
+#include "student.h"
+using namespace std;
+#include<string>
+void student::setName(string first, string last)
+{
+    first = "";
+    last = "";
+}
+
+string student::fullName()
+{
+    string first;
+    string last;
+    string value;
+    value = first.append(last);
+    return value;
+}
+
