95ef17eaf3b38e8f8d13f7951377a5d9c45c227c
Carstar360 <clee124@ecc-jaguar.csuchico.edu>
2019-12-06 10:47:39 -0800

    ok

---
+++ student.h
@@ -0,0 +1,24 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_//Here
+using namespace std;
+#include<string>
+
+class student
+{
+    private:
+    string first;
+    string last;
+    public:
+    void setName(string, string);
+    string fullName();
+};
+
+
+
+
+
+
+
+
+
+#endif//Here
